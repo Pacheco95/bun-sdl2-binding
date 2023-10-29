aec62e22e3a6ba524d477efbc80c8389f9b84c09
Michael Pacheco <mdpgd95@gmail.com>
2023-10-29 00:37:03 -0300

    feat: render triangle

--- lib/functions/renderGeometry.ts
+++ lib/functions/renderGeometry.ts
@@ -11,19 +11,38 @@ export const SDL_RenderGeometry = (
   const indicesPointer = indices ? ptr(Int32Array.from(indices!)) : null;
   const numIndices = indices?.length ?? 0;
 
-  const verticesPointer = new Uint8Array(vertices.length * Vertex.SIZE_BYTES);
+  const buffer = new ArrayBuffer(vertices.length * 20);
+  const view = new DataView(buffer);
 
   let offset = 0;
 
-  for (let vertex of vertices) {
-    verticesPointer.set(vertex.array, offset);
-    offset += Vertex.SIZE_BYTES;
+  for (const vertex of vertices) {
+    view.setFloat32(offset, vertex.position[0], true);
+    offset += 4;
+    view.setFloat32(offset, vertex.position[1], true);
+    offset += 4;
+    
+    const [r, g, b, a] = vertex.color
+
+    view.setUint8(offset, r);
+    offset += 1;
+    view.setUint8(offset, g);
+    offset += 1;
+    view.setUint8(offset, b);
+    offset += 1;
+    view.setUint8(offset, a);
+    offset += 1;
+
+    view.setFloat32(offset, vertex.texCoord[0], true);
+    offset += 4;
+    view.setFloat32(offset, vertex.texCoord[1], true);
+    offset += 4;
   }
 
   const renderResult = foreign(
     renderer,
     texture,
-    ptr(verticesPointer),
+    ptr(buffer),
     vertices.length,
     indicesPointer,
     numIndices,
